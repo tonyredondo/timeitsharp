9d4a78aee8c168998bcc508f4e2956988047c440
Tony Redondo <tony.redondo@datadoghq.com>
2023-03-02 12:31:02 +0100

    Initial DatadogExporter project

---
+++ src/TimeIt.DatadogExporter/TimeItDatadogExporter.cs
@@ -0,0 +1,26 @@
+﻿using DatadogTestLogger.Vendors.Datadog.Trace.Ci;
+using TimeIt.Common.Configuration;
+using TimeIt.Common.Exporter;
+using TimeIt.Common.Results;
+
+namespace TimeIt.DatadogExporter;
+
+public class TimeItDatadogExporter : IExporter
+{
+    /// <inheritdoc />
+    public string Name => "Datadog";
+
+    /// <inheritdoc />
+    public bool Enabled { get; private set; }
+
+    /// <inheritdoc />
+    public void SetConfiguration(Config configuration)
+    {
+        Enabled = true;
+    }
+
+    /// <inheritdoc />
+    public void Export(IEnumerable<ScenarioResult> results)
+    {
+    }
+}
