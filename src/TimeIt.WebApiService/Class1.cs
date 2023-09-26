979eaa1559c3fc20c2f5817580a07d6f30fa26d5
Tony Redondo <tony.redondo@datadoghq.com>
2023-09-26 18:42:04 +0200

    Initial test

---
+++ src/TimeIt.WebApiService/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace TimeIt.WebApiService;
+
+public class Class1
+{
+    public void Method()
+    {
+        var builder = WebApplication.CreateBuilder();
+        var app = builder.Build();
+        app.Run("http://localhost:8126");
+    }
+}
